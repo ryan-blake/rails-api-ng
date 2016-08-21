6ff26a8c40125e6ca6403c7321ac8f7ef6c400e7
Ryan Kliemann <Ryan_blake@icloud.com>
2016-08-21 17:26:36 -0500

    start api adding user to postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+RailsApiNg::Application.config.secret_token = '407cf0280f299a60941d5862bc32cc6141dbe91e939f2afc543f2858379effb8a7b0e1edf6d023af89040270271058921833d86619835960d62d0f1ea5e1a361'
